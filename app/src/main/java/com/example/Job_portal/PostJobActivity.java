4aa9f083986811d6dd1e312c3b7b8857a0a21cbb
sani-1023 <sanitalukder1998@gmail.com>
2020-07-23 16:51:57 +0600

    just checking

--- app/src/main/java/com/example/Job_portal/PostJobActivity.java
+++ app/src/main/java/com/example/Job_portal/PostJobActivity.java
@@ -1,6 +1,8 @@
 package com.example.Job_portal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,10 +10,20 @@ import android.view.View;
 
 import com.example.Job_portal.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PostJobActivity extends AppCompatActivity {
 
     private FloatingActionButton fabBtn;
+
+    private RecyclerView recyclerView;
+    private FirebaseAuth mAuth;
+    private DatabaseReference JobPostDatabase;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,20 @@ public class PostJobActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post_job);
 
         fabBtn=findViewById(R.id.fab_add);
+
+        mAuth=FirebaseAuth.getInstance();
+        FirebaseUser mUser=mAuth.getCurrentUser();
+        String uId = mUser.getUid();
+
+        JobPostDatabase=FirebaseDatabase.getInstance().getReference().child("Job Post").child(uId);
+
+        recyclerView=findViewById(R.id.recycler_job_post_id);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        linearLayoutManager.setReverseLayout(true);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
         fabBtn.setOnClickListener(new View.OnClickListener() {
             @Override
